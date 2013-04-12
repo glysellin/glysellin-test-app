5d0f155b3c2aa927cef07e2b9b5a59663373a6c3
damien <damien@glyph>
2013-04-12 17:48:21 +0200

    changed endpoint

--- config/initializers/restful_sync.rb
+++ config/initializers/restful_sync.rb
@@ -1,12 +1,22 @@
 # encoding: utf-8
 
 RestfulSync.config do |config|
-  # Define the observed resources models
-  config.observed_resources = [Glysellin::Product, Glysellin::Taxonomy, Glysellin::Brand]
+    # Define the observed resources models
+    config.observed_resources = [
+      Glysellin::Product,
+      Glysellin::ProductType, 
+      Glysellin::Brand, 
+      Glysellin::Order, 
+      Glysellin::DiscountCode, 
+      Glysellin::Taxonomy, 
+      Glysellin::ShippingMethod, 
+      Glysellin::PaymentMethod, 
+      User
+    ]
 
-  # Define the accessible resources models
-  config.accessible_resources = [Glysellin::Order]
+    # Define the accessible resources models
+    config.accessible_resources = [Glysellin::Order, User]
 
-  # Define the targeted API URL
-  config.end_point = "localhost:3000/api"
+    # Define the targeted API URL
+    config.end_point = "restful-sync-child.herokuapp.com/api"
 end
