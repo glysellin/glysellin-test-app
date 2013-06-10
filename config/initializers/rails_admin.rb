db84c0d8f9c919a16efd3f5f8b0e56aee22fc36e
vala <gonjo@free.fr>
2013-06-10 02:49:54 +0200

    update glysellin for some fixes and fill rails_admin config

--- config/initializers/rails_admin.rb
+++ config/initializers/rails_admin.rb
@@ -25,6 +25,8 @@ RailsAdmin.config do |config|
 
 
   config.model "Vegetable" do
+    navigation_label "Products"
+
     configure :variant do
       active true
     end
@@ -35,6 +37,8 @@ RailsAdmin.config do |config|
   end
 
   config.model "Fruit" do
+    navigation_label "Products"
+
     configure :variants do
       active true
     end
@@ -44,7 +48,26 @@ RailsAdmin.config do |config|
     end
   end
 
+  [
+    "DiscountType", "OrderAdjustment", "PaymentMethod", "ProductImage",
+    "ProductProperty", "ProductPropertyType", "ShippingMethod"
+  ].each do |model_name|
+    config.model "Glysellin::#{ model_name }" do
+      visible false
+    end
+  end
+
+  [
+    "Glysellin::Brand", "Glysellin::DiscountCode"
+  ].each do |model_name|
+    config.model model_name do
+      navigation_label "Shop"
+    end
+  end
+
   config.model "Glysellin::Product" do
+    visible false
+
     nested do
       exclude_fields :sellable
 
@@ -57,6 +80,8 @@ RailsAdmin.config do |config|
   end
 
   config.model "Glysellin::Variant" do
+    visible false
+
     nested do
       exclude_fields :sellable
 
@@ -84,6 +109,89 @@ RailsAdmin.config do |config|
         end
       end
     end
+  end
+
+  config.model "Glysellin::Order" do
+    navigation_label "Shop"
+
+    object_label_method do
+      :order_name
+    end
+
+    configure :state, :enum do
+      enum do
+        %w(ready paid shipped).map do |s|
+          [I18n.t("glysellin.orders.states.#{ s }"), s]
+        end
+      end
+    end
+
+    configure :payment_method, :string do
+      pretty_value do
+        bindings[:object].payment_method.name if bindings[:object].payment_method
+      end
+    end
+
+    field :ref do
+      read_only true
+    end
+
+    field :total_price do
+      pretty_value do
+        "
+        <strong>
+          #{ bindings[:view] && bindings[:view].number_to_currency(value) }
+        </strong>
+        ".html_safe
+      end
+    end
+
+    include_fields :state, :paid_on, :customer, :billing_address,
+      :shipping_address, :payments, :products
+  end
+
+  config.model "Glysellin::Address" do
+    visible false
+
+    object_label_method do
+      :address_name
+    end
+
+    include_fields :first_name, :last_name, :address, :zip, :city, :country,
+      :tel
+  end
+
+  config.model "Glysellin::LineItem" do
+    visible false
+
+    field :name do
+      read_only true
+    end
+
+    field :price do
+      read_only true
+      pretty_value do
+        bindings[:view] && bindings[:view].number_to_currency(value)
+      end
+    end
+
+    field :quantity do
+      read_only true
+    end
+  end
+
+  config.model "Glysellin::Payment" do
+    visible false
+    include_fields :status, :type, :last_payment_action_on
+  end
+
+  def order_name
+    addr = self.billing_address
+    "#{ self.ref } - #{ addr.first_name } #{ addr.last_name }"
+  end
+
+  def address_name
+    "#{ self.first_name } #{ self.last_name } - #{ self.id }"
   end
 
   def parent_from bindings
