fc1effa3cf29d4d566211ef423785095c42f6bec
vala <gonjo@free.fr>
2013-06-10 02:09:50 +0200

    no Product#display_priority to remove online

--- db/migrate/20130609003613_remove_unused_fields_from_glysellin_products.glysellin_engine.rb
+++ db/migrate/20130609003613_remove_unused_fields_from_glysellin_products.glysellin_engine.rb
@@ -1,15 +1,14 @@
 # This migration comes from glysellin_engine (originally 20130609023300)
 class RemoveUnusedFieldsFromGlysellinProducts < ActiveRecord::Migration
   def up
-    remove_column :glysellin_products, :name, :description, :display_priority,
-      :position, :slug, :sku, :published
+    remove_column :glysellin_products, :name, :description, :position, :slug,
+      :sku, :published
   end
 
   def down
     change_table :glysellin_products do |t|
       t.string :name
       t.text :description
-      t.integer :display_priority
       t.integer :position
       t.string :slug
       t.string :sku
